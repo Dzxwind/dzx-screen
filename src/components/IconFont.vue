7d0c9aa9e2adf69a1ebd65c897740a6aecc6f844
Dzxwind <dzxwind@163.com>
2020-10-20 15:36:54 +0800

    fix(iconfont): render函数改为tempalate 似乎由于render函数的适配不是很好的原因

--- src/components/IconFont.vue
+++ src/components/IconFont.vue
@@ -1,27 +1,10 @@
+<template>
+  <svg class="icon-font" :style="{fontSize: size, color}">
+    <use :xlink:href="'#' + name"></use>
+  </svg>
+</template>
 <script>
-import { h } from 'vue'
 export default {
-  render() {
-    return h(
-      'svg',
-      {
-        class: ['icon-font'],
-        'aria-hidden': true,
-        style: {
-          fontSize: `${this.size}px`,
-          color: this.color
-        }
-      },
-      [
-        h(
-          'use',
-          {
-            'xlink:href': `#${this.name}`
-          }
-        )
-      ]
-    )
-  },
   props: {
     name: {
       type: String
